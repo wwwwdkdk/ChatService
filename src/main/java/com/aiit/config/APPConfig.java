860bcb64f45fbdca3fe0cf86887d88cb043cfe0b
WWWWW\wwwww <wwwwwdkdk@icloud.com>
2023-07-29 17:52:26 +0800

    格式化代码

--- src/main/java/com/aiit/config/APPConfig.java
+++ src/main/java/com/aiit/config/APPConfig.java
@@ -5,8 +5,6 @@ public class APPConfig {
     public static final String resPath = "C:\\Users\\wwwww\\Documents\\res\\";      //需要修改为正确的路径，会影响图片保存功能
     //资源地址头
     public static final String resHttpPath = "http://192.168.31.248:8088/res/";     //需要修改为正确的服务器地址，会影响图片显示功能
-
-
     //图片地址
     public static final String imagePath = resPath + "img/";
     //消息图片地址
